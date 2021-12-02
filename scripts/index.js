82e912da196cf7dd4afd875c53f9643d7e43151a
a.rebrov <a.rebrov@cian.ru>
2021-12-02 20:38:36 +0300

    fix: исправить замечания "надо исправить"

--- scripts/index.js
+++ scripts/index.js
@@ -6,14 +6,14 @@ let profieTitle = document.querySelector('.profile__title');
 let profileSubtitle = document.querySelector('.profile__subtitle');
 
 let editProfileForm = document.querySelector('.popup__container');
-let profieTitleInput = editProfileForm.querySelector('.popup__title-input');
-let profileSubtitleInput = editProfileForm.querySelector('.popup__subtitle-input');
+let profieNameInput = editProfileForm.querySelector('.popup__name-input');
+let profileDescriptionInput = editProfileForm.querySelector('.popup__description-input');
 
 
 function openEditProfilePopup () {
   editProfilePopup.classList.add('popup_opened');
-  profieTitleInput.value = profieTitle.textContent;
-  profileSubtitleInput.value = profileSubtitle.textContent;
+  profieNameInput.value = profieTitle.textContent;
+  profileDescriptionInput.value = profileSubtitle.textContent;
 }
 
 function closeEditProfilePopup () {
@@ -22,10 +22,10 @@ function closeEditProfilePopup () {
 
 function formSubmitHandler (evt) {
   evt.preventDefault();
-  let title = profieTitleInput.value;
-  let subtitle = profileSubtitleInput.value;
-  profieTitle.textContent = title;
-  profileSubtitle.textContent = subtitle;
+  let name = profieNameInput.value;
+  let description = profileDescriptionInput.value;
+  profieTitle.textContent = name;
+  profileSubtitle.textContent = description;
   closeEditProfilePopup();
 }
 
