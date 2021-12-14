d0cc0febbef034d6e86a711fe2e2761b365e451f
a.rebrov <a.rebrov@cian.ru>
2021-12-15 02:17:18 +0300

    feat: добавить открытие формы добавления карточки

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,15 @@
-const editProfilePopup = document.querySelector('.popup');
-const openEditProfileButton = document.querySelector('.profile__edit-button');
-const closeEditProfileButton = document.querySelector('.popup__close-button');
-
 const profileTitle = document.querySelector('.profile__title');
 const profileSubtitle = document.querySelector('.profile__subtitle');
 
-const editProfileForm = document.querySelector('.popup__container');
-const profileNameInput = editProfileForm.querySelector('.popup__name-input');
-const profileDescriptionInput = editProfileForm.querySelector('.popup__description-input');
+const editProfileButton = document.querySelector('.profile__edit-button');
+const editProfilePopup = document.querySelector('.popup_type_profile');
+const profileNameInput = editProfilePopup.querySelector('.popup__name-input');
+const profileDescriptionInput = editProfilePopup.querySelector('.popup__description-input');
+
+const addCardButton = document.querySelector('.card__add-button');
+const addCardPopup = document.querySelector('.popup_type_card');
+const cardNameInput = addCardPopup.querySelector('.popup__card-name-input');
+const cardImageLinkInput = addCardPopup.querySelector('.popup__image-link-input');
 
 const cards = document.querySelector('.cards');
 const cardTemplate = document.querySelector('.card-template').content;
@@ -49,24 +51,35 @@ initialCards.forEach(element => {
 });
 
 function openEditProfilePopup () {
-  editProfilePopup.classList.add('popup_opened');
+  openPopup(editProfilePopup);
   profileNameInput.value = profileTitle.textContent;
   profileDescriptionInput.value = profileSubtitle.textContent;
+  closePopup(editProfilePopup);
 }
 
-function closeEditProfilePopup () {
-  editProfilePopup.classList.remove('popup_opened');
+function openAddCardPopup () {
+  openPopup(addCardPopup);
+  closePopup(addCardPopup);
 }
 
 function formSubmitHandler (evt) {
   evt.preventDefault();
-  let name = profileNameInput.value;
-  let description = profileDescriptionInput.value;
-  profileTitle.textContent = name;
-  profileSubtitle.textContent = description;
-  closeEditProfilePopup();
+  profileTitle.textContent = profileNameInput.value;
+  profileSubtitle.textContent = profileDescriptionInput.value;
+  editProfilePopup.classList.remove('popup_opened');
 }
 
-openEditProfileButton.addEventListener('click', openEditProfilePopup);
-editProfileForm.addEventListener('submit', formSubmitHandler);
-closeEditProfileButton.addEventListener('click', closeEditProfilePopup);
+function openPopup (popup) {
+  popup.classList.add('popup_opened');
+}
+
+function closePopup (popup) {
+  popup.querySelector('.popup__close-button').addEventListener(
+    'click', () => popup.classList.remove('popup_opened')
+  );
+}
+
+editProfileButton.addEventListener('click', openEditProfilePopup);
+editProfilePopup.addEventListener('submit', formSubmitHandler);
+
+addCardButton.addEventListener('click', openAddCardPopup);
