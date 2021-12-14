a8d94361bfe9cb7f846f66c6989ecf2f773b5547
a.rebrov <a.rebrov@cian.ru>
2021-12-14 23:42:46 +0300

    feat: добавить загрузку карточек с картинками при загрузке страницы

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,52 @@
-let editProfilePopup = document.querySelector('.popup');
-let openEditProfileButton = document.querySelector('.profile__edit-button');
-let closeEditProfileButton = document.querySelector('.popup__close-button');
+const editProfilePopup = document.querySelector('.popup');
+const openEditProfileButton = document.querySelector('.profile__edit-button');
+const closeEditProfileButton = document.querySelector('.popup__close-button');
 
-let profileTitle = document.querySelector('.profile__title');
-let profileSubtitle = document.querySelector('.profile__subtitle');
+const profileTitle = document.querySelector('.profile__title');
+const profileSubtitle = document.querySelector('.profile__subtitle');
 
-let editProfileForm = document.querySelector('.popup__container');
-let profileNameInput = editProfileForm.querySelector('.popup__name-input');
-let profileDescriptionInput = editProfileForm.querySelector('.popup__description-input');
+const editProfileForm = document.querySelector('.popup__container');
+const profileNameInput = editProfileForm.querySelector('.popup__name-input');
+const profileDescriptionInput = editProfileForm.querySelector('.popup__description-input');
 
+const cards = document.querySelector('.cards');
+const cardTemplate = document.querySelector('.card-template').content;
+const initialCards = [
+  {
+    name: 'Камчатка',
+    link: './images/kamchatka.jpg'
+  },
+  {
+    name: 'Дивноморск',
+    link: './images/divnomorsk.jpg'
+  },
+  {
+    name: 'Кабардино-Балкария',
+    link: './images/kabardino-balkariya.jpg'
+  },
+  {
+    name: 'Никола-Ленивец',
+    link: './images/nikola-lenivetc.jpg'
+  },
+  {
+    name: 'Петергоф',
+    link: './images/peterhof.jpg'
+  },
+  {
+    name: 'Ольхон',
+    link: './images/olhon.jpg'
+  }
+];
+
+initialCards.forEach(element => {
+  const card = cardTemplate.cloneNode(true);
+
+  card.querySelector('.card__title').textContent = element.name;
+  card.querySelector('.card__image').alt = element.name;
+  card.querySelector('.card__image').src = element.link;
+
+  cards.append(card);
+});
 
 function openEditProfilePopup () {
   editProfilePopup.classList.add('popup_opened');
