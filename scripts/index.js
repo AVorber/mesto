9d18f92e96b30f7e896371181598ec2ec2fc6fd6
a.rebrov <a.rebrov@cian.ru>
2021-12-26 22:42:46 +0300

    feat: закрывать попап при нажатии Escape

--- scripts/index.js
+++ scripts/index.js
@@ -52,12 +52,20 @@ initialCards.forEach(
   element => cards.append(addCard(nameValue = element.name, imageLinkValue = element.link))
 );
 
+function handleKey(evt) {
+  if (evt.key === 'Escape') {
+    closePopup(document.querySelector('.popup_opened'));
+  }
+}
+
 function openPopup(popup) {
   popup.classList.add('popup_opened');
+  document.addEventListener('keydown', handleKey);
 }
 
 function closePopup(popup) {
   popup.classList.remove('popup_opened');
+  document.removeEventListener('keydown', handleKey);
 }
 
 function openEditProfilePopup() {
