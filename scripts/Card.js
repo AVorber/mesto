69fe2c74b23e84a6e52cfa39abe88da2348ebc77
a.rebrov <a.rebrov@cian.ru>
2022-01-25 22:14:28 +0300

    fix: сделать поиск элементов карточки один раз

--- scripts/Card.js
+++ scripts/Card.js
@@ -18,21 +18,18 @@ export class Card {
   }
 
   _setEventListeners() {
-    this._element
-      .querySelector('.card__delete-button')
-      .addEventListener('click', evt => evt.target.closest('.card').remove());
-    this._element
-      .querySelector('.card__favorite-button')
-      .addEventListener('click', evt =>
+    this._cardDeleteButton.addEventListener('click', evt => evt.target.closest('.card').remove());
+    this._cardLikeButton.addEventListener('click', evt =>
         evt.target.classList.toggle('card__favorite-button_active')
       );
-    this._element
-      .querySelector('.card__image')
-      .addEventListener('click', evt => openImagePopup(this._name, this._link));
+    this._cardImage.addEventListener('click', evt => openImagePopup(this._name, this._link));
   }
 
   getCard() {
     this._element = this._getTemplate();
+    this._cardDeleteButton = this._element.querySelector('.card__delete-button');
+    this._cardLikeButton = this._element.querySelector('.card__favorite-button');
+    this._cardImage = this._element.querySelector('.card__image');
     this._setEventListeners();
 
     this._element.querySelector('.card__title').textContent = this._name;
